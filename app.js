28c980dcb4cd847f2efe2b89fcb290c73e03fa77
leaf4monkey <joc98@163.com>
2017-04-20 10:57:57 +0800

    新增测试用例

--- app.js
+++ app.js
@@ -1,62 +1,56 @@
-let http = require('http');
-let fs = require('fs');
-let request = require('request');
+const http = require('http');
+const request = require('request');
 let gollumBase = '';
 //let getGollumUrl = url => `http://localhost:9000${url}`;
 let getGollumUrl = url => `${gollumBase}${url}`;
 
-let checkAuth = req => {
-  return true;
-};
-
-let decode = str => {
-  let map = {};
-  str.split('&').forEach(function(pattern) {
-    let matches = pattern.split('=');
-    map[matches[0]] = decodeURIComponent(matches[1]);
-  });
-  return map;
-};
-
-let encode = map => {
-  let matches = Object.keys(map).map(key => [key, encodeURIComponent(map[key])].join('='));
-  let str = matches.join('&');
-  return str;
+let checkAuth = (req, res) => {
+    return true;
 };
 
 let pipeResponse = function (stream, res) {
-  stream = stream.on('response', function(response) {
-    let {statusCode} = response;
-    if (statusCode >= 300 && statusCode < 400) {
-      let {location} = response.headers;
-      if (location) {
-        response.headers.location = location.replace(gollumBase, '');
-        res.writeHead(response.statusCode, response.headers);
-      }
-    }
-  });
-  stream.pipe(res);
+    stream = stream.on('response', function (response) {
+        let {statusCode} = response;
+        if (statusCode >= 300 && statusCode < 400) {
+            let {location} = response.headers;
+            if (location) {
+                response.headers.location = location.replace(gollumBase, '');
+                res.writeHead(response.statusCode, response.headers);
+            }
+        }
+    });
+    stream.pipe(res);
 };
 
-let server = http.createServer(function(req, res) {
+let server = http.createServer(function (req, res) {
     let {url, method, headers} = req;
-    if (!checkAuth(req)) {
-      return;
+
+    try {
+        checkAuth({url, method, headers});
+    } catch (e) {
+        return res.end(JSON.stringify({
+            error: e.error || e.message,
+            message: e.message,
+            name: e.name,
+            detail: e.details,
+            errorType: e.errorType
+        }));
     }
-    
+
     let gollumUrl = getGollumUrl(url);
 
     pipeResponse(req.pipe(request[method.toLowerCase()](gollumUrl)), res);
 });
 
 module.exports = function (options = {}) {
-  if (options.checkAuth) {
-    checkAuth = options.checkAuth;
-  }
-  gollumBase = options.gollumBase;
-  options.port = options.port || 8000;
-  server.listen(options.port, function () {
-      console.log('gollum-auth-shell started on http://localhost:${options.port}; press ctrl-c to terminate.')
-  });
-  return server;
+    if (options.checkAuth) {
+        checkAuth = options.checkAuth;
+    }
+    gollumBase = options.gollumBase;
+    options.port = options.port || 8000;
+    server.listen(options.port, function () {
+        options.onReady && options.onReady();
+        console.log('gollum-auth-shell started on http://localhost:${options.port}; press ctrl-c to terminate.')
+    });
+    return server;
 };
---
+++ test/index.js
@@ -0,0 +1,59 @@
+/**
+ * Created on 2017/4/20.
+ * @fileoverview 请填写简要的文件说明.
+ * @author joc (Chen Wen)
+ */
+'use strict';
+const http = require('http');
+const request = require('request');
+const faker = require('faker');
+const assert = require('chai').assert;
+
+const proxy = require('../app');
+
+describe('gollum-auth-shell', function () {
+    const methods = ['post', 'get', 'put', 'delete'];
+    const port = 8000, proxyUrl = `http://localhost:${port}`;
+    let mockServer, mockPort = 9000, mockUrl = `http://localhost:${mockPort}`;
+    before(function (done) {
+        mockServer = http.createServer(function (req, res) {
+            let {url, method} = req;
+            res.end(`${method.toUpperCase()} - ${url}`);
+        });
+        mockServer.listen(mockPort, done);
+    });
+    before(function (done) {
+        proxy({
+            port,
+            gollumBase: 'http://localhost:9000',
+            onReady: done,
+            checkAuth: function ({url, method, headers}) {
+                method = method.toLowerCase();
+                if (!new RegExp(method).test(url)) {
+                    throw new Error('request method not match.');
+                }
+            }
+        });
+    });
+
+    it('获得与路径匹配的响应', function (done) {
+        let method = faker.random.arrayElement(methods), url = `/${method}`;
+        request[method](`${proxyUrl}${url}`, function (err, response, body) {
+            assert.equal(body, `${method.toUpperCase()} - ${url}`);
+            done();
+        });
+    });
+
+    it('权限认证失败时获得异常', function (done) {
+        let indexArr = [0, 1, 2, 3];
+        let index1 = faker.random.arrayElement(indexArr);
+        indexArr.splice(indexArr.indexOf(index1), 1);
+        let index2 = faker.random.arrayElement(indexArr);
+
+        let method = methods[index1], url = `/${methods[index2]}`;
+        request[method](`${proxyUrl}${url}`, function (err, response, body) {
+            assert.equal(JSON.parse(body).message, 'request method not match.');
+            done();
+        });
+    });
+});
